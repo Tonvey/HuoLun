afe0757cba7b1cd2ac90c5820135750b6264c940
liangboyan <liangboyanzaz@163.com>
2020-03-08 18:00:34 +0800

    redefine reference count and hobject create

--- include/HuolunCore/HObject.h
+++ include/HuolunCore/HObject.h
@@ -1,20 +1,32 @@
 #pragma once
 #include "HRef.h"
 #include "HTypes.h"
+#include <type_traits>
+#include <cassert>
 class HObject :
     public HRef
 {
-protected:
+public:
     HObject(){}
     HObject(const HObject &)=delete;
     HObject(HObject &&)=delete;
-public:
     virtual ~HObject(){}
-    template<class T>
-    static T *Create()
+    template<class T,class ... Args>
+    static T *Create(Args... args)
     {
-        auto ret = new T();
+        assert((std::is_base_of<HObject,T>::value));
+        auto ret = new T(args...);
         ret->Retain();
         return ret;
     }
+private:
+    void *operator new(size_t size)
+    {
+        return ::operator new(size);
+    }
 };
+template<class T,class ... Args>
+T *CreateObject(Args... args)
+{
+    return HObject::Create<T>(args...);
+}
--- include/HuolunCore/HRef.h
+++ include/HuolunCore/HRef.h
@@ -7,5 +7,7 @@ public:
     virtual ~HRef();
     void Retain();
     void Release();
+    int GetRefCount()const{return mRefCount;}
+private:
     int mRefCount;
 };
